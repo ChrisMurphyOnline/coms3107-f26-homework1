a8939689bf5858e87592715e6a3aa0516ace02c6
Chris Murphy <cdmurphy@seas.upenn.edu>
2026-09-10 06:02:29 -0400

    Add files via upload

---
+++ DonationUtils.java
@@ -0,0 +1,11 @@
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.HashMap;
+import java.util.Map;
+
+public class DonationUtils {
+
+    public static ArrayList<TotalDonation> aggregateDonations(ArrayList<Donation> donations) {
+        return null; // remove this and implement method
+    }
+}
